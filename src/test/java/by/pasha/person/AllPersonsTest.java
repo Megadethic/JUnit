2ea0abb9cab48dfd8f8b96869361dc7ac46e50cc
Pavel Kharitonov <ecclesiastes@tut.by>
2020-03-23 13:21:30 +0300

    added a comment to suite func

--- src/test/java/by/pasha/person/AllPersonsTest.java
+++ src/test/java/by/pasha/person/AllPersonsTest.java
@@ -6,4 +6,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({Person1Test.class, Person2Test.class})
 public class AllPersonsTest {
+    /*
+    Tests with the following files are visible: Test*, *Test, *TestCase.
+    Suite joins tests to run together.
+    Person1Test and Person2Test are not visible but you can run them together here if needs
+    */
 }
